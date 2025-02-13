1216d07d350247a875d249ffb8e2fd49dd93bad8
Joan Gallego Frances <123939267+joangallego@users.noreply.github.com>
2025-02-13 10:10:40 +0000

    Corregido

--- src/js/components/ContadorSegundos.jsx
+++ src/js/components/ContadorSegundos.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-function ContadorSegundos(props) {
-  <div>
-    <p>Este es el secundero: {props.segundos.toString()}</p>
-  </div>
+function ContadorSegundos({ segundos = 0 }) {
+  return (
+    <div>
+      <p>Este es el secundero: {segundos.toString().padStart(6, "0")}</p>
+    </div>
+  );
 }
 export default ContadorSegundos;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import ContadorSegundos from "./components/ContadorSegundos.jsx";
+import ContadorSegundos from "./components/ContadorSegundos";
 
-let variable = 0;
+function App() {
+  const [segundos, setSegundos] = useState(0);
 
-function increment() {
-  setInterval(() => {
-    ReactDOM.createRoot(document.getElementById("app")).render(
-      <>
-        <ContadorSegundos segundos={variable} />
-      </>
-    );
-    variable++;
-  }, 1000);
+  function iniciarContador() {
+    setInterval(() => {
+      setSegundos((prev) => prev + 1);
+    }, 1000);
+  }
+
+  iniciarContador(); 
+
+  return <ContadorSegundos segundos={segundos} />;
 }
 
-increment();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
